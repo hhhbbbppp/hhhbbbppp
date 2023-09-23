a4138484e5fe2ec7b2e5f8a4312bb9dc79b19144
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-09-23 18:55:58 +0900

    Create pj3.cpp

---
+++ C2/pj3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int ii, jj;
+	ii = -12345;
+	jj = -543210;
+	cout << hex << " 0x" << ii << endl;
+	cout << hex << " 0x" << jj << endl;
+}
