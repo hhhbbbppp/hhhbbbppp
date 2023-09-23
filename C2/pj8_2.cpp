ba825166ea55a56e498f20b2dec7d44a04ef993f
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-09-23 20:52:22 +0900

    Create pj8_2.cpp

---
+++ C2/pj8_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// 멤버 변수 int형 bust 추가
+struct student {
+	short age;
+	int height;
+	float weight;
+	int bust;
+};
+
+void main() {
+	student doug, young;
+
+	doug.age = 15;
+	doug.height = 160;
+	doug.weight = 48.5;
+
+	young.age = doug.age + 50;
+	young.height = doug.height + 14;
+	young.weight = doug.weight + 30;
+
+	// sizeof(student) = (2+4+4+4)+2 = 16
+	cout << " " << sizeof(student) << endl;
+	cout << " " << young.weight << endl;
+}
