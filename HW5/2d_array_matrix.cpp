f09e61e4dfa16a12342b0d690161a7a9e093f3a0
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-10-04 21:51:11 +0900

    Update 2d_array_matrix.cpp

--- HW5/2d_array_matrix.cpp
+++ HW5/2d_array_matrix.cpp
@@ -5,17 +5,17 @@ void main() {
 	// A(N*K), B(K*M), C(N*M)
 	int N = 2, K = 3, M = 2;
 	float** A, ** B, ** C;
-	A = new float* [N]; 
-	B = new float* [K]; 
+	A = new float* [N];
+	B = new float* [K];
 	C = new float* [N];
 	for (int n = 0; n < N; n++) A[n] = new float[K]; // 2x3
 	for (int k = 0; k < K; k++) B[k] = new float[M]; // 3x2
 	for (int n = 0; n < N; n++) C[n] = new float[M]; // 2x2
 	A[0][0] = 1; A[0][1] = 2; A[0][2] = 3;
 	A[1][0] = 3; A[1][1] = 2; A[1][2] = 1;
-	B[0][0] = 1; B[0][1] = 1;
+	B[0][0] = 1; B[0][1] = 2;
 	B[1][0] = 3; B[1][1] = 4;
-	B[2][0] = 4; B[2][1] = 5;
+	B[2][0] = 5; B[2][1] = 6;
 	// C = A * B
 	for (int n = 0; n < N; n++) {
 		for (int m = 0; m < M; m++) {
