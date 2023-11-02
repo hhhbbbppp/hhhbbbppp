66a329435c9a71bf8ee6ae0bfb5f16509c8fb85e
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-02 19:33:34 +0900

    Create ppuka.cpp

---
+++ HW9/ppuka.cpp
@@ -0,0 +1,40 @@
+class ppuka {
+  ppuka() {
+    x = 0; y = 0; d = 1.0;
+  }
+  ppuka(float p, float q, float r) {
+    x = p; y = q; d = r;
+  }
+  float x, y, d;
+void moving(){
+  fill(0,0,0);
+  stroke(0,0,0);
+  ellipse(x,y,d*25,d*15);
+ 
+  fill(255,204,153);
+  stroke(255,204,153);
+  ellipse(x,y,d*18,d*12);
+  
+  fill(0,0,0);
+  stroke(0,0,0);
+  circle(x-d*10,y-d*8,d*10);
+  circle(x+d*10,y-d*8,d*10);
+  
+  stroke(0);
+  strokeWeight(5);
+  line(x-d*5,y-4*d,x-3*d,y-3*d);
+  line(x+d*3,y-3*d,x+5*d,y-4*d);
+  
+  stroke(0);
+  strokeWeight(5);
+  line(x-d*7,y-d,x-3*d,y+d);
+  line(x+d*7,y-d,x+3*d,y+d);
+  
+  arc(x,y+4*d,3*d,3*d,0,PI);
+  
+  fill(255,255,0);
+  stroke(255,255,0);
+  circle(x-d*5,y+d*3,d);
+  circle(x+d*5,y+d*3,d);
+}
+}
