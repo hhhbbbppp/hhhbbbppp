c00601e6c445f40c82e5a86c35885071f62696df
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-14 14:21:33 +0900

    Create storeMain2.cpp

---
+++ C8/ex3/storeMain2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "store2.h"
+using namespace std;
+int main() {
+	store A = store(4, 3, 3, 800, 1000, 800);
+	A *= 2; // operator overloading *
+	cout << A.total() << endl;
+	cout << A.apple.many << " " << A.apple.price << endl;
+	cout << A.pear.many << " " <<A.pear.price << endl;
+	cout << A.peach.many << " " << A.peach.price << endl;
+	return 123;
+}
