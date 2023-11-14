475786ed4e01722b06974a3fdfa95eddc77a36ae
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-14 14:24:38 +0900

    Update storeMain2.cpp

--- C8/ex3/storeMain2.cpp
+++ C8/ex3/storeMain2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
 	store A = store(4, 3, 3, 800, 1000, 800);
-	A *= 2; // operator overloading *
+	A *= 2; // operator overloading *=
 	cout << A.total() << endl;
 	cout << A.apple.many << " " << A.apple.price << endl;
 	cout << A.pear.many << " " <<A.pear.price << endl;
