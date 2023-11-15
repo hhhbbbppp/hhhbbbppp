71450ecaac46a4017d4b520c011a3c295d311b85
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-15 16:05:12 +0900

    Create main.cpp

---
+++ C8_1/ex1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "health.h"
+using namespace std;
+int main() {
+	CHealth VIP(65), * mario, VIP2(40), * mickey; //VIP2, mickey 추가
+	mario = new CHealth(70);
+	mickey = new CHealth(20); //mickey age 20
+
+	cout << VIP.age << " " << mario->age << endl;
+	cout << VIP2.age << " " << mickey->age << endl;
+
+	VIP.measureCel();
+	mario->measureCel();
+	VIP2.measureCel();
+	mickey->measureCel();
+
+	cout << VIP.cel << "C " << mario->faren << "F \n";
+	cout << VIP.faren << "F " << mario->cel << "C \n";
+	cout << "size " << sizeof(CHealth) << "\n";
+	getchar();
+	return 1;
+}
