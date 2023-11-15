9ef196e7fb202b2db545246b0aaf8fe2382a7797
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-15 21:01:11 +0900

    Create main.cpp

---
+++ C8_1/ex3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "health2.h"
+using namespace std;
+int main() {
+	CHealth* students = new CHealth[5];
+
+	for (int i = 0; i < 4; i++) {
+		students[i].measure(20 + rand() % 4);
+		(students + i)->run5min();
+	}
+
+	delete[] students;  // delete objects
+	return 123;
+}
