5cc47c14866097f52efb2f5aae91adc19d3df0bd
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-17 17:00:04 +0900

    Create LoLCha.h

---
+++ HW11/method2/LoLCha.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <fstream>
+using namespace std;
+class LoL
+{
+public:
+    LoL(int N1, int N2, float a, float b, float x0, float dx, int m);
+    ofstream dd;
+    int* _N1, * _N2, * _m;
+    float* _a, * _b, * _x0, * _dx;
+    void f0(string);
+    void f1();
+    void f2();
+    ~LoL();
+};
