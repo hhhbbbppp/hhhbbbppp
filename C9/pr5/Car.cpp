f5cfd78dc3c194088a4787b9749f00ab71baacde
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-23 00:56:25 +0900

    Create Car.cpp

---
+++ C9/pr5/Car.cpp
@@ -0,0 +1,31 @@
+#include "Car.h"
+#include <iostream>  //  for cout
+
+Car::Car(float ss, int ww, float pp) {
+	speed = ss;
+	wheels = ww;
+	price = pp;
+	std::cout << " new Car \n";
+}
+void Car::speedUp(float x) { speed += x; }
+void Car::speedDown(float x) { speed -= x; }
+void Car::setPrice(float x) { price = x; }
+float Car::getPrice() { return price; }
+void Car::speedChange(float x) {
+	if (x > 0) speedUp(x);
+	else speedDown(-x);
+}
+void Car::setWheels(int n) { wheels = n; }
+int Car::getWheels() { return wheels; }
+void Car::klaxon(int n) {
+	for (int i = 0; i < n; i++) {
+		std::cout << "  no horn \n";
+	}
+}
+Car Car::operator+(const Car& A) {
+	Car result;
+	result.speed = speed + A.speed;
+	result.wheels = wheels + A.wheels;
+	result.price = price + A.price;
+	return result;
+}
