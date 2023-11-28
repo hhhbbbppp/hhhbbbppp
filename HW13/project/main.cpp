2766a714669ca020975c13802efad4ea21ff5cd6
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-29 00:28:06 +0900

    Update main.cpp

--- HW13/project/main.cpp
+++ HW13/project/main.cpp
@@ -1,14 +1,7 @@
 #include "FIFO.h"
 
 void main() {
-	qq qq(90, 85, 150);
+	qq qq(5, 5, 150);
 	qq.FIFO();
-
-	cout << "Overflow = " << qq.getOverflow() << endl;
-	cout << "Underflow = " << qq.getUnderflow() << endl;
-	cout << "Maximum = " << qq.getMax() << endl;
-	cout << "Average = " << qq.getAvg() << endl;
-	cout << "Lost = " << qq.getLost() << endl;
-	cout << "Loss Rate = " << qq.getLoss() << endl;
-	cout << "Queue Fullness = " << qq.getFull() << "%" << endl;
+	qq.show();
 }
