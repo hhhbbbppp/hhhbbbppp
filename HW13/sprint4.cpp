fc65c0f5e7456085c0f1ea092a6bf449dc4e96c1
hhhbbbppp <144875045+hhhbbbppp@users.noreply.github.com>
2023-11-28 14:13:19 +0900

    Update sprint4.cpp

--- HW13/sprint4.cpp
+++ HW13/sprint4.cpp
@@ -1,75 +1,101 @@
 #include <iostream>
-#include <Windows.h>
-#include <vector>
-#include <fstream>
+#include <Windows.h> // Sleep()
+#include <vector> // vector
+#include <fstream> // file
 using namespace std;
 
-// sprint 4
 class qq {
 public:
 	// constructor
-	qq() {}
 	qq(int i, int o, int c) {
-		NI = i; NO = o; capacity = c;
-		loss = 0; full = 0;
+		NI = i; // ni의 max
+		NO = o; // no의 max
+		capacity = c; // q 용량
+		loss = 0; full = 0; // 초기화
+		max = 0; avg = 0; lost = 0; // 초기화
 	}
 
 	// member variables
 	int NI, NO, capacity;
-	int loss; // Packet loss rate
+	float loss; // Packet loss rate
 	float full; // Queue fullness
+	int max; // maximum
+	int avg; // average
+	int lost; // 손실된 패킷 수
 
 	// member function
 	void FIFO() {
 		vector <int> q;
 
-		int elapsedSeconds = 0, T = 1;
-		int ni, no;
-		int lost = 0;
+		float elapsedSeconds = 0.0, T = 1.0; // 1초
+		int ni, no; // 패킷 생성, 제거 개수 랜덤
 		int overflow = 0;
 		int underflow = 0;
-		int NofPackets[120];
-		int avg = 0;
-		int max = 0;
+		int NofPackets[120]; // 시간에 따른 패킷 수 저장할 array
+		int acc = 0; // accumulate lost
 
-		while (elapsedSeconds < 120) {
-			Sleep(T * 10);
-			elapsedSeconds += T;
+		while (elapsedSeconds < 120) { // 120초 = 2분
+			Sleep(T * 10); // 1초마다 대기
+			elapsedSeconds += T; // 1초씩 증가
 			cout << "Elapsed time[sec] = " << elapsedSeconds << endl;
 
-			ni = rand() % NI;
-			if (capacity < q.size() + ni) {
-				lost += q.size() + ni - capacity;
+			ni = rand() % NI; // random ni
+			if (capacity < q.size() + ni) { // overflow
+				lost += q.size() + ni - capacity; // lost count
 				ni = capacity - q.size();
 				cout << "overflow, size = " << q.size() + ni << endl;
-				overflow += 1;
+				overflow += 1; // overflow count
 			}
-			for (int i = 0; i < ni; i++) {
+			for (int i = 0; i < ni; i++) { // packet 생성
 				q.push_back(rand() % 20);
 			}
 
-			no = rand() % NO;
+			no = rand() % NO; // randomw no
 			for (int i = 0; i < no; i++) {
-				if (q.size() != 0) {
+				if (q.size() != 0) { // packet 제거
 					q.erase(q.begin());
 				}
-				else {
+				else { // underflow
 					cout << "underflow, size = " << q.size() << endl;
-					underflow += 1;
+					underflow += 1; // underflow check
 					break;
 				}
 			}
 
-			NofPackets[elapsedSeconds - 1] = q.size();
-			cout << NofPackets[elapsedSeconds - 1] << endl;
+			NofPackets[static_cast<int>(round(elapsedSeconds)) - 1] = q.size();
+			cout << NofPackets[static_cast<int>(round(elapsedSeconds)) - 1] << endl;
 
+			for (int i = 0; i < elapsedSeconds; i++) {
+				if (max < NofPackets[i]) { // max size 측정
+					max = NofPackets[i]; // 새로운 max 갱신
+				}
+			}
+
+			acc += NofPackets[static_cast<int>(round(elapsedSeconds)) - 1]; // size 계속해서 누적
+			avg = acc / elapsedSeconds;
 			loss = lost / elapsedSeconds;
-			avg += NofPackets[elapsedSeconds - 1];
-			full = (avg / 120.0 / capacity) * 100;
+			full = (acc / elapsedSeconds / capacity) * 100;
+
+			ofstream out("data.txt");
+			for (int i = 0; i < elapsedSeconds; i++) {
+				out << NofPackets[i] << endl;
+			}
 		}
 	}
 
-	int getLoss() const {
+	int getMax() const {
+		return max;
+	}
+
+	int getAvg() const {
+		return avg;
+	}
+	
+	int getLost() const {
+		return lost;
+	}
+
+	float getLoss() const {
 		return loss;
 	}
 
@@ -79,9 +105,12 @@ public:
 };
 
 void main() {
-	qq qq1(35, 30, 100);
-	qq1.FIFO();
+	qq qq(90, 5, 100);
+	qq.FIFO();
 
-	cout << qq1.getLoss() << endl;
-	cout << qq1.getFull() << endl;
+	cout << "Maximum = " << qq.getMax() << endl;
+	cout << "Average = " << qq.getAvg() << endl;
+	cout << "Lost = " << qq.getLost() << endl;
+	cout << "Loss Rate = " << qq.getLoss() << "%" << endl;
+	cout << "Queue Fullness = " << qq.getFull() << "%" << endl;
 }
